4e756ad11533f7f2bc437ff495a6541a9bed4b50
AlfaHusniMubarok <49012736+AlfaHusniMubarok@users.noreply.github.com>
2019-05-24 20:16:37 +0700

    Add files via upload

---
+++ TA_MesinKasir/TA_MesinKasir/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TA_MesinKasir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HP Pavilion 15-AK050TX",
+            "Acer Nitro 5 V15-8300H",
+            "DELL Inspiron 15-5577-7300HQ",
+            "Asus F507ZD",
+            "MSI GL62M 7RDX 1018"});
+            this.comboBox1.Location = new System.Drawing.Point(192, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Masukan Jenis Laptop";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(192, 157);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(190, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "Masukan Jumlah";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 184);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(557, 276);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(352, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(557, 361);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(352, 29);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(557, 419);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(352, 52);
+            this.textBox4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Hasil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(557, 477);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(799, 477);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Keluar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(921, 516);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ TA_MesinKasir/TA_MesinKasir/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TA_MesinKasir
+{
+    public partial class Form1 : Form
+     {
+        int pilihan, harga, totalHarga, jumlahBarang, kembalian, uang = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pilihan = comboBox1.SelectedIndex;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                harga = 9359000;
+                jumlahBarang = comboBox2.SelectedIndex + 1;
+                totalHarga = harga * jumlahBarang;
+                textBox1.Text = totalHarga.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                harga = 10559000;
+                jumlahBarang = comboBox2.SelectedIndex + 1;
+                totalHarga = harga * jumlahBarang;
+                textBox1.Text = totalHarga.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                harga = 10970000;
+                jumlahBarang = comboBox2.SelectedIndex + 1;
+                totalHarga = harga * jumlahBarang;
+                textBox1.Text = totalHarga.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                harga = 11640000;
+                jumlahBarang = comboBox2.SelectedIndex + 1;
+                totalHarga = harga * jumlahBarang;
+                textBox1.Text = totalHarga.ToString();
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                harga = 12999000;
+                jumlahBarang = comboBox2.SelectedIndex + 1;
+                totalHarga = harga * jumlahBarang;
+                textBox1.Text = totalHarga.ToString();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox2.Text, out uang);
+            kembalian = uang - totalHarga;
+            textBox3.Text = kembalian.ToString();
+            if (uang > totalHarga)
+            {
+                textBox4.Text = "Terimakasih telah berbelanja di toko Kami, Semoga Anda Senang :-) #TokoLaptop";
+            }
+            else
+            {
+                textBox4.Text = "Maaf, nominal uang yang anda masukan kurang. Silahkan masukan nominal uang Anda kembali";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                textBox1.Text = String.Empty;
+                textBox2.Text = String.Empty;
+                textBox3.Text = String.Empty;
+                textBox4.Text = String.Empty;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
